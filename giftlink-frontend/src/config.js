459b2b2e7374e488bc28a587e6521fe251db7dc6
MrTokata <panhaseth453@gmail.com>
2026-01-27 22:44:59 -0500

    fetch gift from backend

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -2,5 +2,7 @@ const config = {
   backendUrl: process.env.REACT_APP_BACKEND_URL,
 };
 
+console.log(config.ba)
+
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
 export {config as urlConfig}
